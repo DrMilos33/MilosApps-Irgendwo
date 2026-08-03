5d81e7be82d7386f0e3842d7994df95a55afe5d9
DrMilos33 <305868979+DrMilos33@users.noreply.github.com>
2026-08-03 16:37:31 +0200

    Deploy somewhere-now DEV from ca3edb72c966da5c35e537ad869e3419686a57c9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "somewhere-now-shell-v12";
+const CACHE_NAME = "somewhere-now-shell-v13";
 const APP_BASE_URL = new URL("./", self.registration.scope);
 const appUrl = (relativePath = "") => new URL(relativePath, APP_BASE_URL).href;
 const LIVE_METADATA_PATHS = new Set([
